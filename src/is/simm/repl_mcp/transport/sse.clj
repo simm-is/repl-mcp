141ca743b57dfa1f14427746c90383d037a27319
pat killean <pat.killean@gmail.com>
2025-12-06 18:11:09 -0400

    heartbeat

--- src/is/simm/repl_mcp/transport/sse.clj
+++ src/is/simm/repl_mcp/transport/sse.clj
@@ -59,11 +59,40 @@
 
 (defn channel-send! [channel event data]
   (let [message (str "event: " event "\ndata: " data "\n\n")]
-    (log/log! {:level :info :msg "Sending SSE message" 
-               :data {:event event :data data :message message}})
+    (when-not (= event "ping")
+      (log/log! {:level :info :msg "Sending SSE message"
+                 :data {:event event :data data :message message}}))
     (http-kit/send! channel message false)))
 
 (defonce connections (atom {}))
+
+;; Heartbeat mechanism
+(def heartbeat-interval 15000)
+(defonce heartbeat-running? (atom false))
+(defonce heartbeat-future (atom nil))
+
+(defn start-heartbeat-loop! []
+  (reset! heartbeat-running? true)
+  (reset! heartbeat-future
+    (future
+      (log/log! {:level :info :msg "Starting SSE heartbeat loop"})
+      (loop []
+        (when @heartbeat-running?
+          (Thread/sleep heartbeat-interval)
+          (doseq [[session-id session-data] @connections]
+            (try
+              (channel-send! (:session/channel session-data) "ping" "{}")
+              (catch Exception e
+                (log/log! {:level :error :msg "Error sending heartbeat"
+                           :data {:session-id session-id :error (.getMessage e)}}))))
+          (recur))))))
+
+(defn stop-heartbeat-loop! []
+  (reset! heartbeat-running? false)
+  (when-let [f @heartbeat-future]
+    (future-cancel f)
+    (reset! heartbeat-future nil))
+  (log/log! {:level :info :msg "Stopped SSE heartbeat loop"}))
 
 (defn new-session-id [] 
   (str (java.util.UUID/randomUUID)))
@@ -105,7 +134,7 @@
       (http-kit/as-channel req
                            {:on-open
                             (fn [channel]
-                              (log/log! {:level :info :msg "SSE connection opened" 
+                              (log/log! {:level :info :msg "SSE connection opened"
                                          :data {:session-id session-id}})
                               (let [mcp-context (context-factory)
                                     {:session/keys [send!]} (assoc-session!
@@ -116,7 +145,7 @@
                                 (send! "endpoint" (str "/messages/" session-id))))
                             :on-close
                             (fn [_channel status]
-                              (log/log! {:level :info :msg "SSE connection closed" 
+                              (log/log! {:level :info :msg "SSE connection closed"
                                          :data {:status status :session-id session-id}})
                               (dissoc-session! session-id))}))))
 
@@ -140,11 +169,13 @@
 
 (defn start-http-server! [context-factory port]
   (log/log! {:level :info :msg "Starting HTTP+SSE server" :data {:port port}})
+  (start-heartbeat-loop!)
   (let [handler (create-ring-handler context-factory)]
     (http-kit/run-server handler {:port port :legacy-return-value? false})))
 
 (defn stop-http-server! [server]
   (when server
     (log/log! {:level :info :msg "Stopping HTTP+SSE server"})
+    (stop-heartbeat-loop!)
     (http-kit/server-stop! server {:timeout 100})
     (reset! connections {})))
